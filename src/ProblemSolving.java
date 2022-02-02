f4d6b535b37d0fe9eef97a83939f01f14124855f
kuehar <green95251@gmail.com>
2022-02-03 06:05:07 +0900

    added countBits to ProblemSolving.java.

--- src/ProblemSolving.java
+++ src/ProblemSolving.java
@@ -5,7 +5,7 @@ public class ProblemSolving {
 	
 	
 	/*
-	 * This function is for searching smallest number which is not to the sum of a subset of elements  of the array.  
+	 * This function is for searching smallest number which is not to the sum of a subset of elements of the array.  
 	 * N is the length of array, Time complexity is O(nlogn)
 	 */
 	public static int smallestNonconstructibleValue(List<Integer> A) {
@@ -18,5 +18,19 @@ public class ProblemSolving {
 	        maxConstructibleValue += a;
 	    }    
 	    return maxConstructibleValue+1;
-	}	
+	}
+	
+	
+	/*
+	 * to count the number of bits that are set to 1 in an integer is a good way to get up to speed with primitive type
+	 * Time complexity is O(n).
+	 */
+	public static short countBits(int x) {
+		short numBits = 0;
+		while(x != 0) {
+			numBits += (x & 1);
+			x >>>= 1;
+		}
+		return numBits;
+	}
 }
