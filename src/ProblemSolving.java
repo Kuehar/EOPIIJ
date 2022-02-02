bf437cea194ef9286683550cec43211b65482c72
kuehar <green95251@gmail.com>
2022-02-02 18:51:19 +0900

    fixed existed java source

--- src/ProblemSolving.java
+++ src/ProblemSolving.java
@@ -2,6 +2,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class ProblemSolving {
+	
+	
+	/*
+	 * This function is for searching smallest number which is not to the sum of a subset of elements  of the array.  
+	 * N is the length of array, Time complexity is O(nlogn)
+	 */
 	public static int smallestNonconstructibleValue(List<Integer> A) {
 	    Collections.sort(A);
 	    int maxConstructibleValue = 0;
