16e44a5de4506ce7482e6d2c4f29b92ff3aedae1
kuehar <green95251@gmail.com>
2022-02-02 18:43:46 +0900

    fixed existed java source

--- src/ProblemSolving.java
+++ src/ProblemSolving.java
@@ -1,4 +1,17 @@
+import java.util.Collections;
+import java.util.List;
 
 public class ProblemSolving {
-
+	public static int smallestNonconstructibleValue(List<Integer> A) {
+	    Collections.sort(A);
+	    int maxConstructibleValue = 0;
+	    for(int a: A){
+	        if(a > maxConstructibleValue+1){
+	            break;
+	        }
+	    }    
+	    return maxConstructibleValue+1;
+	}
+	
+	
 }
