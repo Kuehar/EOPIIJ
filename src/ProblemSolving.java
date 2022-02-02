3f18504686d7255118b11104b989f605e57c9c66
kuehar <green95251@gmail.com>
2022-02-03 07:04:47 +0900

    added improved parity to ProblemSolving.java.

--- src/ProblemSolving.java
+++ src/ProblemSolving.java
@@ -24,6 +24,7 @@ public class ProblemSolving {
 	/*
 	 * to count the number of bits that are set to 1 in an integer is a good way to get up to speed with primitive type
 	 * Time complexity is O(n).
+	 *　>>> is expression of bit shift to right in Java.
 	 */
 	public static short countBits(int x) {
 		short numBits = 0;
@@ -36,13 +37,32 @@ public class ProblemSolving {
 	
 	/*
 	 * Straightforward implementation of parity check.parity checks are used to detect single bit errors in data storage and communication.
-	 */
+	 * ^= is expression of XOR
+	 * &= is expression of AND
+	 * Time complexity is O(n)
+	 
 	public static short parity(long x) {
 		short result = 0;
 		while(x != 0) {
-			result ^= 1;
-			x &= (x-1); // Drops the lowest set bit of x.
+			result ^= (x & 1);
+			x >>>= 1;
 		}
 		return result;
 	}
+	*/
+	
+	/*
+	 * more improved version in the best-case and avarage cases than previous one.
+	 */
+	
+	public static short parity(long x) {
+		short result = 0;
+		while(x != 0) {
+			result ^= 1; // this line is improved
+			x &= (x-1); // Drrops the lowest set bit of x.
+		}
+		return result;
+			
+	}
+	
 }
