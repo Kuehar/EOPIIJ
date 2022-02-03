f965719b76668bb30d7e3babed493dd8e97aa59b
kuehar <green95251@gmail.com>
2022-02-04 07:12:32 +0900

    added plusOne to Arrays.java.

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,3 +1,4 @@
+import java.util.List;
 
 public class Arrays {
 	
@@ -17,4 +18,25 @@ public class Arrays {
 			}
 		}
 	}
+	
+	/*
+	 * Function which takes as input an array of digits encoding a decimal number D and updates the array to represent the number D+1
+	 * Time complexity is O(N), where n is the length of A
+	 * 
+	 */
+	public static List<Integer> plusOne(List<Integer> A){
+		int n = A.size()-1;
+		A.set(n,A.get(n)+1);
+		for(int i=n;i>0 && A.get(i) == 10; --i) {
+			A.set(i, 0);
+			A.set(i-1,A.get(i-1)+1);
+		}
+		if(A.get(0) == 10) {
+			// Need additional digit as the most significant digit (i.e., A.get(0))
+			// has a carry-out.
+			A.set(0,0);
+			A.set(0,1);
+			}
+		return A;
+		}
 }
