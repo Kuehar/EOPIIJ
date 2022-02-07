cab3e4e986362f6ed80ed98e2208d3d9822d4f23
kuehar <green95251@gmail.com>
2022-02-07 21:58:57 +0900

    added dutchFlagPartition to arraySolution.java.

--- src/ArraysSolution.java
+++ src/ArraysSolution.java
@@ -179,4 +179,33 @@ public class ArraysSolution {
 		}
 		return primes;
 	}	
+	/*
+	 * Program that takes an Array A and an index i into A, and rearranges the elements suchh that all elements less than A[i] appear first, followed by elements equal to the pivot,followed by elements greater than pivot.
+	 */
+	
+	public static enum Color{RED,WHITE,BLUE}
+	
+	public static void dutchFlagPartition(int pivotIndex,List<Color> A) {
+		Color pivot = A.get(pivotIndex);
+		// First pass: group elements smaller than pivot
+		for(int i=0;i<A.size();++i) {
+			// Look for a smaller element.
+			for(int j=i+1;j<A.size();++j) {
+				if(A.get(i).ordinal() < pivot.ordinal()) {
+					Collections.swap(A,i,j);
+					break;
+				}
+			}
+		}
+		// Second pass: group elements larger than pivot.
+		for(int i=A.size()-1;i>=0 && A.get(i).ordinal() >= pivot.ordinal();--i) {
+			// Look for a larger element. Stop when we reach an element less than pivot. since first pass has moved them to the start of A.
+			for(int j=i-1;j>=0 && A.get(j).ordinal() >= pivot.ordinal();--j) {
+				if(A.get(j).ordinal() > pivot.ordinal()) {
+					break;
+				}
+			}
+		}
+	}
+	
 }
