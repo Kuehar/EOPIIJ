c64483b1b4d821d05b1a58aad7033f0977047f87
kuehar <green95251@gmail.com>
2022-02-08 21:38:46 +0900

    added applyPermutation to arraysSolution.java

--- src/ArraysSolution.java
+++ src/ArraysSolution.java
@@ -208,4 +208,19 @@ public class ArraysSolution {
 		}
 	}
 	
+	public static void applyPermutation(List<Integer> perm,List<Integer> A) {
+		for(int i=0;i<A.size();++i) {
+			int next = i;
+			while(perm.get(next)>=0) {
+				Collections.swap(A, i,perm.get(i));
+				int temp = perm.get(next);
+				perm.set(next, perm.get(next)-perm.size());
+				next = temp;
+			}
+		}
+		for(int i=0;i<perm.size();i++) {
+			perm.set(i, perm.get(i)+perm.size());
+		}
+	}
+	
 }
