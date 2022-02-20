ca1f1b3ac7f34d9923ce1e751e7b57a472a4f80c
kuehar <green95251@gmail.com>
2022-02-21 08:53:16 +0900

    remove squareEven to arraysSolution.java

--- src/ArraysSolution.java
+++ src/ArraysSolution.java
@@ -267,30 +267,4 @@ public class ArraysSolution {
 		}
 		return pascalTriangle;
 	}
-	public int[] squareEven(int[] array,int length){
-		// Check for edge cases.
-		if(array == null) {
-			return null;
-		}
-		
-		// Create a resultant Array which would hold the result.
-		int[] result = new int[length];
-		
-		// Iterate through the original Array.
-		for(int i=0;i<length;i++) {
-			// Get the element from slot i  of the input Array.
-			int element = array[i];
-			
-			// if the index is an even number, then we need to square element.
-			if(i%2==0) {
-				element*=element;
-			}
-			
-			// Write element int the result Array.
-			result[i] = element;
-		}
-		return result;
-	}
-
-
 }
